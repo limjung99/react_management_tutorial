2abeec93bebda249e6efe83cc9bdbe425c53df50
Lim junghwan <limjung99@naver.com>
2021-12-23 08:00:33 +0900

    Create Customer.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,53 @@
-import logo from './logo.svg';
+
 import './App.css';
 import { Component } from 'react';
-import { render } from '@testing-library/react';
+import Customer from './components/Customer'
 
+
+const customers =[ {
+  'id':1,
+  'image': 'https://placeimg.com/64/64/1',
+  'name' : '임정환',
+  'birthday': '990822',
+  'gender' : '남자',
+  'job' : '대학생'
+},
+{
+  'id':2,
+  'image': 'https://placeimg.com/64/64/2',
+  'name' : '이명박',
+  'birthday': '990822',
+  'gender' : '여자',
+  'job' : '대통령'
+},
+{
+  'id':3,
+  'image': 'https://placeimg.com/64/64/3',
+  'name' : '노무현',
+  'birthday': '990822',
+  'gender' : '남자',
+  'job' : '힙합가수'
+}
+]
 class App extends Component {
   render(){
     return (
-      <div className="gray-background">
-        <img src={logo} lat='logo' />
-        <h2>Let's develop management system!</h2>
+      <div>
+        {
+          customers.map(c=>{
+            return(
+              <Customer
+                key={c.id}
+                id={c.id}
+                image={c.image}
+                name={c.name}
+                birthday={c.birthday}
+                gender={c.gender}
+                job={c.job}
+                /> //for문과 비슷 
+            )
+          })
+        }
       </div>
     );
   }
